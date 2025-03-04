4f78f44726c93ff1ab5b223d9d897c4a2864050c
juliachaidez3 <jmchaidez@cpp.edu>
2025-03-03 18:31:04 -0800

    Finished knn problem

--- Problem 3/knn.py
+++ Problem 3/knn.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Julia Chaidez
+# FILENAME: knn
+# SPECIFICATION: Read the file email_classification.csv and compute the LOO-CV error rate for a 1NN classifier on the spam/ham classification task
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1 hour
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -15,42 +15,50 @@ import csv
 db = []
 
 #Reading the data in a csv file
-with open('binary_points.csv', 'r') as csvfile:
+with open('email_classification.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
          db.append (row)
 
+#Count misclassified instances
+misclassified = 0
+numSamples = len(db)
+
+#Convert each feature value to float to avoid warning messages
+for i in range(len(db)):
+    db[i][:20] = [float(value) for value in db[i][:20]]
+
 #Loop your data to allow each instance to be your test set
-for i in db:
+for i in range(numSamples):
+    X = []
+    Y = []
 
-    #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration.
-    #For instance, X = [[1, 2, 3, 4, 5, ..., 20]].
-    #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
+    #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration
+    for j in range(numSamples):
+        if j != i:
+           X.append(db[j][:20]) # Attributes
+           Y.append(db[j][20])  # Class label
 
-    #Transform the original training classes to numbers and add them to the vector Y.
-    #Do not forget to remove the instance that will be used for testing in this iteration.
-    #For instance, Y = [1, 2, ,...].
-    #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
-
-    #Store the test sample of this iteration in the vector testSample
-    #--> add your Python code here
+    #Store the test sample
+    testSample = db[i][:20]
+    trueLabel = db[i][20]
 
     #Fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
     clf = clf.fit(X, Y)
 
-    #Use your test sample in this iteration to make the class prediction. For instance:
-    #class_predicted = clf.predict([[1, 2, 3, 4, 5, ..., 20]])[0]
-    #--> add your Python code here
+    #Use your test sample in this iteration to make the class prediction
+    classPredicted = clf.predict([testSample])[0]
 
-    #Compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
+    #Compare the prediction with the true label of the test instance to start calculating the error rate
+    if classPredicted != trueLabel:
+       misclassified += 1
+    
+errorRate = misclassified / numSamples
 
 #Print the error rate
-#--> add your Python code here
+print("The LOO-CV error rate for a 1NN classifier on the spam/ham classification task is", errorRate)
 
 
 
